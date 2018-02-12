dbdac429dfa7a4e88550c6fc0a9c31b58a490242
Drew Hoener <drewhoener@gmail.com>
2018-02-12 13:02:41 -0500

    Merge of a pull request by ishanyang on the upstream

--- jquery.longpress.js
+++ jquery.longpress.js
@@ -14,13 +14,19 @@
  * Copyright (c) 2008-2013, Vaidik Kapoor (kapoor [*dot*] vaidik -[at]- gmail [*dot*] com)
  */
 
-(function($) {
-    $.fn.longpress = function(longCallback, shortCallback, duration) {
+(function ($) {
+    $.fn.longpress = function (longCallback, shortCallback, duration) {
+
+        var mobile = false;
+        if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+            mobile = true;
+        }
+
         if (typeof duration === "undefined") {
             duration = 500;
         }
 
-        return this.each(function() {
+        return this.each(function () {
             var $this = $(this);
 
             // to keep track of how long something was pressed
@@ -39,7 +45,7 @@
                 var context = $(this);
 
                 // set a timeout to call the longpress callback when time elapses
-                timeout = setTimeout(function() {
+                timeout = setTimeout(function () {
                     if (typeof longCallback === "function") {
                         longCallback.call(context, e);
                     } else {
@@ -53,16 +59,16 @@
                 mouse_up_x = e.originalEvent.touches ? e.originalEvent.touches[0].clientX : e.originalEvent.clientX;
                 mouse_up_y = e.originalEvent.touches ? e.originalEvent.touches[0].clientY : e.originalEvent.clientY;
 
-                if(Math.abs(mouse_up_x - mouse_down_x) > 100 || Math.abs(mouse_up_y - mouse_down_y) > 100) return;
+                if (Math.abs(mouse_up_x - mouse_down_x) > 100 || Math.abs(mouse_up_y - mouse_down_y) > 100) return;
 
-                if(Date.now() - mouse_down_time >= duration) return;
+                if (Date.now() - mouse_down_time >= duration) return;
 
                 clearTimeout(timeout);
                 if (typeof shortCallback === "function") {
 
                     // prevent double-firing for touch devices
                     // (mouseup, touchend)
-                    if(Date.now() - short_call_time > 50) {
+                    if (Date.now() - short_call_time > 50) {
                         shortCallback.call($(this), e);
                     }
                     short_call_time = Date.now();
@@ -78,15 +84,17 @@
                 clearTimeout(timeout);
             }
 
+            if (bool) {
+                // Mobile Support
+                $this.on('touchstart', mousedown_callback);
+                $this.on('touchend', mouseup_callback);
+                $this.on('touchmove', move_callback);
+                return;
+            }
             // Browser Support
             $this.on('mousedown', mousedown_callback);
             $this.on('mouseup', mouseup_callback);
             $this.on('mousemove', move_callback);
-
-            // Mobile Support
-            $this.on('touchstart', mousedown_callback);
-            $this.on('touchend', mouseup_callback);
-            $this.on('touchmove', move_callback);
         });
     };
 }(jQuery));
